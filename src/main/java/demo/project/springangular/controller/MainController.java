26e667f4772c55ab843dc73a477c4f6ad34b3b01
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-04-02 01:43:57 +0300

    vid6 JWT add getMapping to MainController

--- src/main/java/demo/project/springangular/controller/MainController.java
+++ src/main/java/demo/project/springangular/controller/MainController.java
@@ -1,6 +1,14 @@
 package demo.project.springangular.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class MainController {
 //todo   Make main controller here!!!
+    @GetMapping("/")
+     public String hello(){
+        return "hello";
+     }
 }
 
