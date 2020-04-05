c240ea69a09e473b231075c58ccb07dc68d1d52f
Yurii Krynytskyi <yuri.krynytsky@gmail.com>
2020-04-05 19:02:10 +0300

    change to PostController

--- src/main/java/demo/project/springangular/controller/PostController.java
+++ src/main/java/demo/project/springangular/controller/PostController.java
@@ -1,16 +1,35 @@
 package demo.project.springangular.controller;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import demo.project.springangular.dto.PostDto;
+import demo.project.springangular.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/posts/")
 public class PostController {
 
-//    @PostMapping
-//     public void (){
-// todo start video 6 from 9:16
-//     }
+    @Autowired
+    private PostService postService;
+
+    @PostMapping
+    public ResponseEntity createPost(@RequestBody PostDto postDto) {
+        postService.createPost(postDto);
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
+    @GetMapping("/all")
+    public ResponseEntity<List<PostDto>> showAllPosts() {
+        return new ResponseEntity<>(postService.showAllPosts(), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    public ResponseEntity<PostDto> getSinglePost(@PathVariable @RequestBody Long id) {
+        return new ResponseEntity<>(postService.readSinglePost(id), HttpStatus.OK);
+    }
 }
 
